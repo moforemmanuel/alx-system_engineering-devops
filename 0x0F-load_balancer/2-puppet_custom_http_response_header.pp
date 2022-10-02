90c0c65054afa06995a2a1ccd885a95662c22329
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-02 02:18:19 +0100

    nginx load balance header config with puppet fix 3

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,7 @@
 #add custom header to nginx req/res headers, to identify which server is selected by the HAProxy
 
-exec {'cHeader':
+exec {'header':
 	provider => shell,
-	path => '/usr/bin:/usr/sbin:/bin',
 	command => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo sed -i '/^\tserver_name/a \\n\tadd_header X-Served-By $HOSTNAME;\n' /etc/nginx/sites-available/default ; sudo service nginx restart',
 }
 
