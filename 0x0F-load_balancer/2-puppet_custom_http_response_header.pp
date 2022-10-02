707ac7d46cfef65dc1e09ddcc0bdbc45ddd57bb5
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-02 02:10:11 +0100

    nginx load balance header config with puppet fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,6 +2,7 @@
 
 exec {'set_haproxy_header':
 	provider => shell,
+	path => '/usr/bin:/usr/sbin:/bin',
 	command => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo sed -i '/^\tserver_name/a \\n\tadd_header X-Served-By $HOSTNAME;\n' /etc/nginx/sites-available/default ; sudo service nginx restart',
 }
 
