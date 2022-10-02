bb175d38f89b002bbc761599e593320651c88faa
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-02 02:23:39 +0100

    nginx load balance header config with puppet fix 5 mich21

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,12 @@
 #add custom header to nginx req/res headers, to identify which server is selected by the HAProxy
 
-exec {'header':
-	provider => shell,
-	command => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo sed -i "/^\tserver_name/a \\n\tadd_header X-Served-By $HOSTNAME;\n" /etc/nginx/sites-available/default ; sudo service nginx restart',
-}
-
 #exec {'header':
-#  provider    => shell,
-#  command     => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; header_str="location \/ {\n\t\tadd_header X-Served-By \$hostname;" ; sudo sed -i "s/location \/ {/$header_str/" /etc/nginx/sites-available/default ; sudo service nginx restart',
+#	provider => shell,
+#	command => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo sed -i "/^\tserver_name/a \\n\tadd_header X-Served-By $HOSTNAME;\n" /etc/nginx/sites-available/default ; sudo service nginx restart',
 #}
+
+exec {'header':
+  provider    => shell,
+  command     => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; header_str="location \/ {\n\t\tadd_header X-Served-By \$hostname;" ; sudo sed -i "s/location \/ {/$header_str/" /etc/nginx/sites-available/default ; sudo service nginx restart',
+}
 
