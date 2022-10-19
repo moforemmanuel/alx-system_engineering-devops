3365b380e3873593404cc9ab61386419f2aab461
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-19 18:05:09 +0100

    get api data fix 14

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,23 +5,17 @@
 import requests
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    endpoint = "https://jsonplaceholder.typicode.com/"
     userId = argv[1]
-    completed_tasks: list = []
-    user_name = requests\
-        .get(f'https://jsonplaceholder.typicode.com/users/{userId}')\
-        .json().get('name')
-    tasks = requests.\
-        get(f'https://jsonplaceholder.typicode.com/todos?userId={userId}')\
-        .json()
-    # pprint(type(tasks))
-    # pprint(user_name)
-
-    for task in tasks:
+    user = requests.get(endpoint + "users/{}".
+                        format(userId)).json()
+    todo = requests.get(endpoint + "todos?userId={}".
+                        format(userId)).json()
+    completed_tasks = []
+    for task in todo:
         if task.get('completed') is True:
             completed_tasks.append(task.get('title'))
-
-    print(f'{user_name} is done with tasks \
-    ({len(completed_tasks)}/{len(tasks)}):')
-    for task in completed_tasks:
-        print(f"\t {task}")
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
