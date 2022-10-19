88ed6bd8e6bfb22848c85d81cabe47f3fe71ed61
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-19 17:16:00 +0100

    get api data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,15 +8,15 @@ import sys
 
 completed_tasks: list = []
 user_name = requests.get(f'https://jsonplaceholder.typicode.com/users/\
-{sys.argv[1]}').json()['name']
+{sys.argv[1]}').json().get('name')
 tasks = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId=\
 {sys.argv[1]}').json()
 # pprint(type(tasks))
 # pprint(user_name)
 
 for task in tasks:
-    if task['completed'] is True:
-        completed_tasks.append(task['title'])
+    if task.get('completed') is True:
+        completed_tasks.append(task.get('title'))
 
 print(f'{user_name} is done with tasks ({len(completed_tasks)}/{len(tasks)}):')
 for task in completed_tasks:
