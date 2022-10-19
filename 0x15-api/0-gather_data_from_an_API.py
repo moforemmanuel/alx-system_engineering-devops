5b532532749e841cfc1d0699439748a1d8c856fe
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-19 17:26:48 +0100

    get api data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,18 +6,23 @@ Gather data from an API
 import requests
 import sys
 
-completed_tasks: list = []
-user_name = requests.get(f'https://jsonplaceholder.typicode.com/users/\
-{sys.argv[1]}').json().get('name')
-tasks = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId=\
-{sys.argv[1]}').json()
-# pprint(type(tasks))
-# pprint(user_name)
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    completed_tasks: list = []
+    user_name = requests\
+        .get(f'https://jsonplaceholder.typicode.com/users/{userId}')\
+        .json().get('name')
+    tasks = requests.\
+        get(f'https://jsonplaceholder.typicode.com/todos?userId={userId}')\
+        .json()
+    # pprint(type(tasks))
+    # pprint(user_name)
 
-for task in tasks:
-    if task.get('completed') is True:
-        completed_tasks.append(task.get('title'))
+    for task in tasks:
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
 
-print(f'{user_name} is done with tasks ({len(completed_tasks)}/{len(tasks)}):')
-for task in completed_tasks:
-    print(f'\t{task}')
+    print(f'{user_name} is done with tasks \
+    ({len(completed_tasks)}/{len(tasks)}):')
+    for task in completed_tasks:
+        print(f"\t{task}")
