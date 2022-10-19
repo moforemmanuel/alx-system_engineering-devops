026aadbec3ecbc70f95e1c05e80063d7ad29b543
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-19 17:51:29 +0100

    get api data fix 6

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,50 @@
 #!/usr/bin/python3
-"""Gather data from an API
+"""getting data from an api
 """
 
-# from pprint import pprint
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    userId = sys.argv[1]
-    completed_tasks: list = []
-    user_name = requests\
-        .get(f'https://jsonplaceholder.typicode.com/users/{userId}')\
-        .json().get('name')
-    tasks = requests.\
-        get(f'https://jsonplaceholder.typicode.com/todos?userId={userId}')\
-        .json()
-    # pprint(type(tasks))
-    # pprint(user_name)
-
-    for task in tasks:
+if __name__ == '__main__':
+    endpoint = "https://jsonplaceholder.typicode.com"
+    userId = argv[1]
+    user = requests.get(endpoint + "users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get(endpoint + "todos?userId={}".
+                        format(userId), verify=False).json()
+    completed_tasks = []
+    for task in todo:
         if task.get('completed') is True:
             completed_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
 
-    print(f'{user_name} is done with tasks \
-    ({len(completed_tasks)}/{len(tasks)}):')
-    for task in completed_tasks:
-        print(f"\t {task}")
+# #!/usr/bin/python3
+# """Gather data from an API
+# """
+#
+# # from pprint import pprint
+# import requests
+# import sys
+#
+# if __name__ == "__main__":
+#     userId = sys.argv[1]
+#     completed_tasks: list = []
+#     user_name = requests\
+#         .get(f'https://jsonplaceholder.typicode.com/users/{userId}')\
+#         .json().get('name')
+#     tasks = requests.\
+#         get(f'https://jsonplaceholder.typicode.com/todos?userId={userId}')\
+#         .json()
+#     # pprint(type(tasks))
+#     # pprint(user_name)
+#
+#     for task in tasks:
+#         if task.get('completed') is True:
+#             completed_tasks.append(task.get('title'))
+#
+#     print(f'{user_name} is done with tasks \
+#     ({len(completed_tasks)}/{len(tasks)}):')
+#     for task in completed_tasks:
+#         print(f"\t {task}")
