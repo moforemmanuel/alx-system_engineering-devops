66d0b6aa64e6ee116a105c8d3a68d7ff371a5207
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-11-05 01:22:18 +0100

    feat: get subscribers for subreddit fix 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""return the number of subscribers of a reddit sub"""
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """function"""
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
     import requests
 
-    data = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/about.json",
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False)
-
-    if data.status_code >= 300:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-    return data.json().get("data").get("subscribers")
+
+    return sub_info.json().get("data").get("subscribers")
